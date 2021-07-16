fe8e68e4e02d79d4da3af64a421afc0f6f6a7bb9
malonw <malonw@pacbell.net>
2021-07-16 16:27:11 -0700

    messing around with sttore

--- Python_Stack/python/OOP/Store_and_Products/Store.py
+++ Python_Stack/python/OOP/Store_and_Products/Store.py
@@ -6,7 +6,6 @@ class Products:
         self.catagory = catagory
         
         
-        
     
     def update_price(self,percent_change, is_increased):
         if is_increased == True:
@@ -31,36 +30,51 @@ class Store:
     def __init__ (self, name, products=[]):
         self.name = name
         self.prods=products
-        id(self.prods)
+        self.price = Products
         
         
-    def add_product(self, new_product):
+    def add_product(self, new_product, catagory, price):
+        self.catagory = catagory
+        self.price = price
         self.new_product = new_product
         self.prods.append(new_product)
         return self
     
     def sell_product(self,ind):
         self.ind =ind  
-        if id == ind:
-            self.prods.pop(id)
+        for i in range(len(self.prods)):
+            if i == ind:
+                self.prods.pop(ind)
+                return
         return self
 
     def inflation(self,percent_increase):
-        # self.percent_increased = percent_increase
-        # self.percent_increased = Products.is_increased
-        pass
+        self.percent_increased = percent_increase
+        for i in self.prods:
+            self.price = self.price + (self.price * percent_increase)
+            print(self.price)
+        return self
+        
+        
+
 
     def set_clearance(self,catagory,percent_discount):
-        # self.percent_discount=percent_discount
-        # if self.catagory == catagory:
-        #     Products.price = Products.price - (Products.price * percent_discount)
-        pass
+        self.percent_discount=percent_discount
+        if self.catagory == catagory:
+            self.price = self.price - (self.price * percent_discount)
+        return self
+        # pass
 
 
 Toy=Store("RcVehicles")
-Toy.add_product("Erevo").add_product("RedOct").add_product("MiniCoop").add_product("MustangGT").add_product("RamTruck4x4")
+Toy.add_product("Erevo", "RcTruck", 500).add_product("RedOct", "RcSub", 200).add_product("MiniCoop", "RcCar", 100).add_product("MustangGT", "RcCar", 700).add_product("RamTruck4x4","RcTruck", 600)
 print(Toy.prods)
 Toy.sell_product(2)
+print(Toy.prods)
+Toy.inflation(.50)
+Toy.set_clearance("RcSub", .80)
+print(Toy.catagory)
+
 
 
 
