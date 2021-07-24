76961c314b0477bf4d8699b6c72e3806a3942833
Malon Wright <malonw@pacbell.net>
2021-07-24 14:51:23 -0700

    updated NinjaGold Game

--- Python_Stack/django/Django_Projects/Ninja_Gold_Proj/ninja_gold_app/urls.py
+++ Python_Stack/django/Django_Projects/Ninja_Gold_Proj/ninja_gold_app/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('gold_generator',views.gold_generator),
-
+    path('process_money', views.process_money),
+    path('reset', views.reset),
+    
 ]
