980b457bcf98f02f95cc24e71c6175f3641cccd3
malonw <malonw@pacbell.net>
2021-07-26 09:48:31 -0700

    ??

--- Python_Stack/django/Django_Projects/Ninja_Gold_Proj/ninja_gold_app/views.py
+++ Python_Stack/django/Django_Projects/Ninja_Gold_Proj/ninja_gold_app/views.py
@@ -13,25 +13,25 @@ def gold_generator(request):
         if request.POST['action'] == "farm":
             total = random.randint(10,20)
             request.session["total_gold"] += total
-            request.session["activities"].append('Received ' + str(total) + ' gold from the farm!('+ strftime("%a, %d %b %Y %H:%M %p")+')', localtime())
+            request.session["activities"].append(f'Received {str(total)} gold from the farm!({ strftime("%a, %d %b %Y %H:%M %p")})', localtime())
             
         elif request.POST['action'] == "cave":
             total = random.randint(5,10)
             request.session["total_gold"] += total
-            request.session["activities"].append('Received ' + str(total) + ' gold from the cave!('+ strftime("%a, %d %b %Y %H:%M %p")+')', localtime())
+            request.session["activities"].append(f'Received {str(total)} gold from the cave!({ strftime("%a, %d %b %Y %H:%M %p")})', localtime())
             
         elif request.POST["action"] == "house":
             total = random.randint(2,5)
             request.session["total_gold"] += total
-            request.session["activities"].append('Received ' + str(total) + ' gold from the house!('+ strftime("%a, %d %b %Y %H:%M %p")+')', localtime())
+            request.session["activities"].append(f'Received {str(total)} gold from the house!({ strftime("%a, %d %b %Y %H:%M %p")})', localtime())
             
         elif request.POST["action"] == "casino":
             total = random.randint(-50,50)
             request.session["total_gold"] += total
             if total > 0:
-                request.session["activities"].append('Entered a Casino and won ' + str(total) + ' gold!('+ strftime("%a, %d %b %Y %H:%M %p")+')', localtime())
+                request.session["activities"].append(f'Entered a Casino and won {str(total)} gold!({ strftime("%a, %d %b %Y %H:%M %p")})', localtime())
             else:
-                request.session["activities"].append('Entered a Casino and lost ' + str(total) + ' gold!('+ strftime("%a, %d %b %Y %H:%M %p")+')', localtime())
+                request.session["activities"].append(f'Entered a Casino and lost {str(total)} gold!({ strftime("%a, %d %b %Y %H:%M %p")})', localtime())
             
     else:
         return redirect('/')
