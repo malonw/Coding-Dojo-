1d3354a3f198e9ee353f3db305544f2b04ae545c
malonw <malonw@pacbell.net>
2021-07-30 16:57:37 -0700

    updated TVshows app

--- Python_Stack/django/django_orm/Orm_Projects/Semi_Restful_TV_Shows/TV_shows_app/models.py
+++ Python_Stack/django/django_orm/Orm_Projects/Semi_Restful_TV_Shows/TV_shows_app/models.py
@@ -4,9 +4,11 @@ from django.db.models.fields import DateTimeField
 class Show(models.Model):
     title = models.CharField(max_length=255)
     network = models.CharField(max_length=255)
-    release_date = models.DateField(default='01-01-2021')
+    release_date = models.DateField(auto_now=True)
     descript = models.TextField(null=True)
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
+    def __repr__(self):
+        return f"({self.id}){self.title}{self.network}{self.release_date}{self.descript} "
 
 # Create your models here.
--- Python_Stack/django/django_orm/Orm_Projects/Semi_Restful_TV_Shows/TV_shows_app/views.py
+++ Python_Stack/django/django_orm/Orm_Projects/Semi_Restful_TV_Shows/TV_shows_app/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, redirect
 from .models import Show
 
-def shows(request):
+
+def tvshows(request):
     context = {
-        'all_shows':Show.objects.all()
-        }
-    
-    return render(request, 'shows.html', context)
+        'all_shows': Show.objects.all()
+    }
+
+    return render(request, 'tvshows.html', context)
+
 
 def new(request):
     if request.POST == "POST":
@@ -15,27 +17,38 @@ def new(request):
             network=request.POST['netwrok'],
             release_date=request.POST['release_date'],
             descript=request.POST['descript']
-            )
+        )
     return render(request, 'create.html')
 
-def update(request):
-    if request.method == "POST":
-        if Show.objects.get(id=request.POST["id"]):
-            Show.save()
-            return render(request, 'read.html')
-    return render(request, 'edit.html')
 
-def read_one(request):
-        if request.method == 'GET':
-            if Show.objects.get(id=request.POST["id"]):
-                return render (request, 'read.html')        
-        return render(request, 'read.html')
 
-def delete(request):
-    if request.method == 'POST':
-        if Show.objects.get(id=request.POST["id"]):
-            Show.delete()
-        return redirect("/")
-    else:
-        return redirect ("/")
-# Create your views here.
+
+def edit(request, id):
+    edit_query = {
+        'shows': Show.objects.get(id=id)
+    }
+    return render(request, 'edit.html', edit_query)
+
+
+def update(request, id):
+    update = Show.objects.get(id=id)
+    update.title = request.POST['title'],
+    update.network = request.POST['netwrok'],
+    update.release_date = request.POST['release_date'],
+    update.descript = request.POST['descript'],
+    update.save()
+    return redirect('/')
+
+
+def read_one(request, id):
+    show_query = {
+        'shows': Show.objects.get(id=id)
+    }
+    return render(request, 'read.html', show_query)
+
+
+
+def delete(request, id):
+    dele=Show.objects.get(id=id)
+    dele.delete()
+    return redirect('/')
