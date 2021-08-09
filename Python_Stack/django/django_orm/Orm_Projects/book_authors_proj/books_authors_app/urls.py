b09eab90310c285ce6a7abee82a46051da6a308b
malonw <malonw@pacbell.net>
2021-08-09 16:47:31 -0700

    working on books and authors

--- Python_Stack/django/django_orm/Orm_Projects/book_authors_proj/books_authors_app/urls.py
+++ Python_Stack/django/django_orm/Orm_Projects/book_authors_proj/books_authors_app/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('add_book', views.add_book),
+    path('view_book/<int:id>', views.view_book),
+    path('authors', views.authors_index),
+    path('add_author', views.add_author),
+    path('view_author/<int:id>', views.view_author),
+
 ]
--- Python_Stack/django/django_orm/Orm_Projects/book_authors_proj/books_authors_app/views.py
+++ Python_Stack/django/django_orm/Orm_Projects/book_authors_proj/books_authors_app/views.py
@@ -1,6 +1,72 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import Book, Author
+
 
 def index(request):
-        return render(request, "index.html")
+    context = {
+        'all_books': Book.objects.all()
+    }
+
+    return render(request, "index.html", context)
+
+
+def add_book(request):
+    if request.method == 'POST':
+        errors1 = Book.objects.book_validator(request.POST)
+    if len(errors1) > 0:
+        for key, value in errors1.items():
+            messages.error(request, value, extra_tags=key)
+        return redirect('/')
+
+    else:
+        Book.objects.create(
+            title=request.POST['title'],
+            desc=request.POST['desc'],
+        )
+    return redirect('/')
+
+
+def view_book(request, id):
+    view_book_query = {
+        'book': Book.objects.get(id=id),
+        'books': Book.book_authors.through.objects.filter(id=id),
+
+    }
+    return render(request, 'view_one_book.html', view_book_query)
+
+
+def authors_index(request):
+    author_query = {
+        'all_authors': Author.objects.all()
+    }
+    return render(request, "authors.html", author_query)
+
+
+def add_author(request):
+    if request.method == 'POST':
+        errors = Author.objects.author_validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value, extra_tags=key)
+        return redirect('/authors')
+
+    else:
+        Author.objects.create(
+            fname=request.POST['fname'],
+            lname=request.POST['lname'],
+            notes=request.POST['desc'],
+        )
+    return redirect('/authors')
+
+
+def view_author(request, id):
+    view_author_query = {
+        'author': Author.objects.get(id=id),
+        'books': Book.book_authors.through.objects.filter(id=id),
+
+    }
+    return render(request, 'view_one_author.html', view_author_query)
+
 
 # Create your views here.
