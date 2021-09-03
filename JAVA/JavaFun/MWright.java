0f5390f16fe188c5a48f4c2d313c20901a64f09b
Malon Wright <83512002+malonw@users.noreply.github.com>
2021-09-02 17:03:10 -0700

    Working on Java

---
+++ JAVA/JavaFun/MWright.java
@@ -0,0 +1,8 @@
+public class MWright {
+    public static void main(String[] args) {
+        System.out.println("My name is Malon Wright");
+        System.out.println("I am 49 until October");
+        System.out.println("My hometown is Oakley, Ca");
+    }
+
+}
